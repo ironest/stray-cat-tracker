e7fd55e0657d16078898d9f09c8738a4924d5bf3
ironest <ironest@gmail.com>
2019-10-08 22:57:52 +1100

    Added CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get  "/cats", to: "cats#home", as: "cats"
+
+  post "/cats", to: "cats#create"
+  get  "/cats/new", to: "cats#new", as: "new_cat"
+
+  get  "/cats/:id", to: "cat#show", as: "cat"
+
+  put "/cats/:id", to: "cats#update"
+  get "/cats/:id/edit", to: "cats#edit", as: "edit_cats"
+
+  delete "/cats/:id", to: "cats#destroy"
+  get "/cats/:id/delete", to: "cats#delete", as: "delete_cats"
+
+
+
 end
